b1e9d9ae156868d5a609d7daa278868156ebd8ca
Alex Wilson <alex.wilson@joyent.com>
2016-11-09 20:25:58 -0800

    joyent/ufds-terror-alert#1 want warnings when risk of terror in UFDS is heightened

---
+++ bin/verifylog.js
@@ -0,0 +1,163 @@
+#!/usr/bin/env node
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2016, Joyent, Inc.
+ */
+
+var mod_fs = require('fs');
+var mod_byline = require('byline');
+var mod_dashdash = require('dashdash');
+var mod_path = require('path');
+var mod_cproc = require('child_process');
+var mod_crypto = require('crypto');
+
+var options = [
+	{
+		names: ['yubikey', 'y'],
+		type: 'bool',
+		help: 'Use secret from yubikey'
+	},
+	{
+		names: ['secret', 's'],
+		type: 'string',
+		help: 'Supply secret as hex string'
+	},
+	{
+		names: ['help', 'h'],
+		type: 'bool',
+		help: 'Shows this help text'
+	}
+];
+
+if (require.main !== module)
+	return;
+
+var parser = mod_dashdash.createParser({ options: options });
+
+try {
+	var opts = parser.parse(process.argv);
+} catch (e) {
+	console.error('verifylog: error: %s', e.message);
+	process.exit(1);
+}
+
+if (opts.help || opts._args.length !== 1 || (!opts.yubikey && !opts.secret)) {
+	var help = parser.help({}).trimRight();
+	console.error('verifylog: verify a hash-chain signed UFDS log');
+	console.error('usage: ./verifylog [options] ufds_log.jsonl\n');
+	console.error(help);
+	console.error('one of the --yubikey|-y or --secret|-s options must ' +
+	    'be supplied');
+	process.exit(1);
+}
+
+var logFile = opts._args[0];
+var log = mod_fs.createReadStream(logFile);
+var linest = new mod_byline.LineStream();
+linest.encoding = 'utf-8';
+log.pipe(linest);
+
+var challenge, secret, psecret, phmac, lineno = 1;
+
+linest.on('readable', readMore);
+linest.on('end', function () {
+	console.error('Log validated ok');
+	process.exit(0);
+});
+
+function readMore() {
+	var line;
+	while ((line = linest.read()) !== null) {
+		var obj = JSON.parse(line);
+		if (secret === undefined && challenge === undefined) {
+			challenge = new Buffer(obj.challenge, 'hex');
+			if (opts.secret) {
+				var h = mod_crypto.createHmac('sha1',
+				    new Buffer(opts.secret, 'hex'));
+				h.update(challenge);
+				secret = h.digest();
+			} else if (opts.yubikey) {
+				var args = ['-2', '-H', '-x'];
+				args.push(challenge.toString('hex'));
+				console.error('If YubiKey is flashing, ' +
+				    'please press the button');
+				var copts = {};
+				copts.stdio = ['pipe', 'pipe', 'inherit'];
+				var res = mod_cproc.spawnSync('ykchalresp',
+				    args, copts);
+				if (res.status !== 0)
+					process.exit(1);
+				var out = res.stdout.toString('ascii').trim();
+				secret = new Buffer(out, 'hex');
+			}
+		} else {
+			verifyLogEntry(obj);
+		}
+		++lineno;
+	}
+}
+
+function nextSecret(sec) {
+	var h = mod_crypto.createHash('sha1');
+	h.update(sec);
+	return (h.digest());
+}
+
+function verifyLogEntry(obj) {
+	var step = 0, valid = false;
+
+	var hmac = obj.hmac;
+	var ourHmac;
+	delete (obj.hmac);
+	var blob = new Buffer(JSON.stringify(obj), 'utf-8');
+
+	var h = mod_crypto.createHmac('sha256', secret);
+	h.update(blob);
+	ourHmac = h.digest().toString('base64');
+	valid = (ourHmac == hmac);
+
+	if (!valid && psecret !== undefined) {
+		var h = mod_crypto.createHmac('sha256', psecret);
+		h.update(blob);
+		ourHmac = h.digest().toString('base64');
+		if (ourHmac == hmac) {
+			if (phmac === hmac)
+				return;
+			console.error('WARNING: re-used hash chain value for ' +
+			    'non-matching entries at line %d', lineno);
+			return;
+		}
+	}
+
+	psecret = secret;
+	secret = nextSecret(secret);
+	++step;
+
+	while (step < 5 && !valid) {
+		var h = mod_crypto.createHmac('sha256', secret);
+		h.update(blob);
+		ourHmac = h.digest().toString('base64');
+		valid = (ourHmac == hmac);
+		psecret = secret;
+		secret = nextSecret(secret);
+		++step;
+	}
+
+	if (!valid) {
+		console.error('Line %d failed verification:', lineno);
+		console.error(obj);
+		console.error('Included HMAC   = %s', hmac);
+		console.error('Calculated HMAC = %s (at step %d)', ourHmac,
+		    step);
+		process.exit(1);
+	}
+
+	if (step > 1) {
+		console.error('WARNING: entry appears to have been elided ' +
+		    'between lines %d and %d', lineno - 1, lineno);
+	}
+	phmac = hmac;
+}
---
+++ server.js
@@ -0,0 +1,147 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2016, Joyent, Inc.
+ */
+
+var mod_fs = require('fs');
+var mod_path = require('path');
+var mod_sqlite = require('sqlite3').verbose();
+var mod_vasync = require('vasync');
+var mod_crypto = require('crypto');
+var mod_bunyan = require('bunyan');
+var mod_mailer = require('nodemailer');
+
+var Notifier = require('./lib/notifier');
+var createUfdsPool = require('./lib/ufds-pool');
+var UfdsWatcher = require('./lib/ufds-watcher');
+
+var configFile = mod_path.join(__dirname, 'etc', 'config.json');
+var config = JSON.parse(mod_fs.readFileSync(configFile).toString('utf-8'));
+
+var db = new mod_sqlite.Database(config.sqlite_db);
+
+var log = mod_bunyan.createLogger({
+	name: 'ufds-terror-alert',
+	level: 'debug'
+});
+
+var initsec = mod_path.join(__dirname, 'etc', 'initialsecret');
+
+try {
+	mod_fs.statSync(initsec);
+	throw (new Error('Initial secret file must be removed'));
+} catch (ex) {
+	if (ex.code !== 'ENOENT') {
+		throw (new Error('Initial secret file must be removed'));
+	}
+}
+
+var stmt = db.prepare(
+    'select name from sqlite_master where type = ? and name = ?');
+
+mod_vasync.pipeline({
+	funcs: [
+		checkCreateMetadata, checkCreateUsers, checkCreateKeys
+	]
+}, function (err) {
+	if (err)
+		throw (err);
+	stmt.finalize();
+
+	var opts = {
+		config: config,
+		db: db,
+		log: log
+	};
+	opts.mailer = mod_mailer.createTransport(config.smtp);
+	opts.notifier = new Notifier(opts);
+	opts.ufdsPool = createUfdsPool(opts);
+	opts.watcher = new UfdsWatcher(opts);
+
+	setInterval(function () {
+		opts.watcher.check();
+	}, 1000);
+});
+
+function checkCreateMetadata(_, cb) {
+	stmt.all('table', 'metadata', function (err, rows) {
+		if (err) {
+			cb(err);
+			return;
+		}
+		if (rows.length < 1) {
+			db.run('create table metadata (' +
+			    'key text primary key, value text)', function () {
+				doInitialSecret(cb);
+			});
+		} else {
+			cb();
+		}
+	});
+}
+
+function doInitialSecret(cb) {
+	var secret = mod_crypto.randomBytes(20);
+	var challenge = mod_crypto.randomBytes(32);
+	var ins = db.prepare('insert into metadata (key, value) values ' +
+	    '(?, ?)');
+	var h = mod_crypto.createHmac('sha1', secret);
+	h.update(challenge);
+	ins.run('challenge', challenge.toString('base64'));
+	ins.run('secret', h.digest().toString('base64'));
+	ins.run('serial', '-1');
+	mod_fs.writeFileSync(initsec, secret.toString('hex') + '\n');
+	var initl =
+	    JSON.stringify({ challenge: challenge.toString('hex') }) + '\n';
+	mod_fs.writeFileSync(config.ufds_log, initl);
+	log.warn('ufds log root secret written out to %s: please copy this ' +
+	    'value to offline storage and delete this file now', initsec);
+	var int = setInterval(function () {
+		try {
+			mod_fs.statSync(initsec);
+		} catch (ex) {
+			if (ex.code === 'ENOENT') {
+				clearInterval(int);
+				config.initialSync = true;
+				cb();
+			}
+		}
+	}, 1000);
+}
+
+function checkCreateUsers(_, cb) {
+	stmt.all('table', 'users', function (err, rows) {
+		if (err) {
+			cb(err);
+			return;
+		}
+		if (rows.length < 1) {
+			db.run('create table users (' +
+			    'uuid text primary key, login text, ' +
+			    'userpassword text, email text, ' +
+			    'operator integer)', cb);
+		} else {
+			cb();
+		}
+	});
+}
+
+function checkCreateKeys(_, cb) {
+	stmt.all('table', 'keys', function (err, rows) {
+		if (err) {
+			cb(err);
+			return;
+		}
+		if (rows.length < 1) {
+			db.run('create table keys (' +
+			    'uuid text, fingerprint text,' +
+			    'name text, comment text, ' +
+			    'primary key (uuid, fingerprint))', cb);
+		} else {
+			cb();
+		}
+	});
+}
